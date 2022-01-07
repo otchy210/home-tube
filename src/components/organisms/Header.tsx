26c84d330c19085898a51fb1a5b64c93dbc51a34
Otchy <id+github@otchy.net>
2022-01-07 23:22:28 +0900

    0.0.6: Make header more responstive

--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Button, Container, Form, FormControl, Nav, Navbar } from 'react-bootstrap';
 import Icon from '../../images/icon.svg';
+import Logo from '../../images/logo.svg';
 
 const Header: React.FC = () => {
     return (
-        <Navbar bg="dark" variant="dark" fixed="top" expand="sm">
+        <Navbar bg="light" variant="light" className="border-bottom" fixed="top" expand="sm">
             <Container fluid>
                 <Navbar.Brand href="#" className="me-auto">
-                    <Icon />
+                    <Icon className="d-inline d-md-none" style={{ height: '32px' }} />
+                    <Logo className="d-none d-md-inline" style={{ height: '32px' }} />
                 </Navbar.Brand>
                 <Navbar.Toggle />
                 <Navbar.Collapse className="justify-content-end mt-3 mt-sm-0">
