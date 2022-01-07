5a071f03af5f556902d8d8311c8ec76d166894ff
Otchy <id+github@otchy.net>
2022-01-08 00:05:57 +0900

    0.0.9: Add search icon

--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Button, Container, Form, FormControl, Nav, Navbar } from 'react-bootstrap';
 import Icon from '../../images/icon.svg';
 import Logo from '../../images/logo.svg';
+import Search from '../../images/search.svg';
 import Config from '../../images/config.svg';
 
 const iconStyle = { height: '32px' };
@@ -11,21 +12,22 @@ const Header: React.FC = () => {
         <Navbar bg="light" variant="light" className="border-bottom" fixed="top" expand="sm">
             <Container fluid>
                 <Navbar.Brand href="#" className="me-auto">
-                    <Icon className="d-inline d-md-none" style={iconStyle} />
-                    <Logo className="d-none d-md-inline" style={iconStyle} />
+                    <Icon className="d-inline d-sm-none" style={iconStyle} />
+                    <Logo className="d-none d-sm-inline" style={iconStyle} />
                 </Navbar.Brand>
                 <Navbar.Toggle />
                 <Navbar.Collapse className="justify-content-end mt-3 mt-sm-0">
                     <Nav>
                         <Form className="d-flex">
                             <FormControl type="search" />
-                            <Button variant="primary" className="ms-2">
-                                Search
+                            <Button variant="primary" className="ms-2 text-nowrap">
+                                <Search style={{ height: '22px' }} />
+                                <span className="d-inline d-sm-none d-md-inline ms-2 ms-sm-0 ms-lg-2 align-middle">Search</span>
                             </Button>
                         </Form>
                     </Nav>
                     <Nav>
-                        <Nav.Link href="#features" className="ms-1">
+                        <Nav.Link href="#features" className="ms-1 py-0">
                             <Config style={iconStyle} />
                             <span className="d-inline d-sm-none d-lg-inline ms-2 ms-sm-0 ms-lg-2 align-middle">Config</span>
                         </Nav.Link>
