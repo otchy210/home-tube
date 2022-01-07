699e0ae13120a21bcc809357bb2bfe5e0331e289
Otchy <id+github@otchy.net>
2022-01-07 23:50:07 +0900

    0.0.8: Add config icon

--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -2,14 +2,17 @@ import React from 'react';
 import { Button, Container, Form, FormControl, Nav, Navbar } from 'react-bootstrap';
 import Icon from '../../images/icon.svg';
 import Logo from '../../images/logo.svg';
+import Config from '../../images/config.svg';
+
+const iconStyle = { height: '32px' };
 
 const Header: React.FC = () => {
     return (
         <Navbar bg="light" variant="light" className="border-bottom" fixed="top" expand="sm">
             <Container fluid>
                 <Navbar.Brand href="#" className="me-auto">
-                    <Icon className="d-inline d-md-none" style={{ height: '32px' }} />
-                    <Logo className="d-none d-md-inline" style={{ height: '32px' }} />
+                    <Icon className="d-inline d-md-none" style={iconStyle} />
+                    <Logo className="d-none d-md-inline" style={iconStyle} />
                 </Navbar.Brand>
                 <Navbar.Toggle />
                 <Navbar.Collapse className="justify-content-end mt-3 mt-sm-0">
@@ -23,7 +26,8 @@ const Header: React.FC = () => {
                     </Nav>
                     <Nav>
                         <Nav.Link href="#features" className="ms-1">
-                            Config
+                            <Config style={iconStyle} />
+                            <span className="d-inline d-sm-none d-lg-inline ms-2 ms-sm-0 ms-lg-2 align-middle">Config</span>
                         </Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
