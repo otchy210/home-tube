c419c8217f03d5eaf11fced8a2c25daab8224970
Otchy <id+github@otchy.net>
2022-01-08 01:45:05 +0900

    0.0.11: Make Header works with react-router

--- src/components/organisms/Header.test.tsx
+++ src/components/organisms/Header.test.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import Header from './Header';
+import { BrowserRouter } from 'react-router-dom';
 
 describe('Header', () => {
     it('renders properly', () => {
-        const { container } = render(<Header />);
+        const { container } = render(
+            <BrowserRouter>
+                <Header />
+            </BrowserRouter>
+        );
         expect(container).toMatchSnapshot();
     });
 });
--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -1,36 +1,60 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { LinkContainer } from 'react-router-bootstrap';
 import { Button, Container, Form, FormControl, Nav, Navbar } from 'react-bootstrap';
 import Icon from '../../images/icon.svg';
 import Logo from '../../images/logo.svg';
 import Search from '../../images/search.svg';
 import Config from '../../images/config.svg';
+import { createSearchParams, useNavigate } from 'react-router-dom';
 
 const iconStyle = { height: '32px' };
 
 const Header: React.FC = () => {
+    const queryRef = useRef<HTMLInputElement>(null);
+    const navigate = useNavigate();
+    const doSearch = () => {
+        const params = createSearchParams();
+        params.append('names', queryRef.current?.value ?? '');
+        navigate(`/search${params.get('names') !== '' ? `?${params.toString()}` : ''}`);
+    };
+    const onQueryKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+        if (e.key === 'Enter') {
+            e.preventDefault();
+            doSearch();
+            return;
+        }
+    };
+    const onSearchSubmit = (e: React.BaseSyntheticEvent) => {
+        e.preventDefault();
+        doSearch();
+    };
     return (
         <Navbar bg="light" variant="light" className="border-bottom" fixed="top" expand="sm">
             <Container fluid>
-                <Navbar.Brand href="#" className="me-auto">
-                    <Icon className="d-inline d-sm-none" style={iconStyle} />
-                    <Logo className="d-none d-sm-inline" style={iconStyle} />
-                </Navbar.Brand>
+                <LinkContainer to="/">
+                    <Navbar.Brand className="me-auto">
+                        <Icon className="d-inline d-sm-none" style={iconStyle} />
+                        <Logo className="d-none d-sm-inline" style={iconStyle} />
+                    </Navbar.Brand>
+                </LinkContainer>
                 <Navbar.Toggle />
                 <Navbar.Collapse className="justify-content-end mt-3 mt-sm-0">
                     <Nav>
-                        <Form className="d-flex">
-                            <FormControl type="search" />
-                            <Button variant="primary" className="ms-2 text-nowrap">
+                        <Form className="d-flex" onSubmit={onSearchSubmit}>
+                            <FormControl type="search" onKeyDown={onQueryKeyDown} ref={queryRef} />
+                            <Button variant="primary" className="ms-2 text-nowrap" onClick={onSearchSubmit}>
                                 <Search style={{ height: '22px' }} />
                                 <span className="d-inline d-sm-none d-md-inline ms-2 ms-sm-0 ms-lg-2 align-middle">Search</span>
                             </Button>
                         </Form>
                     </Nav>
                     <Nav>
-                        <Nav.Link href="#features" className="ms-1 py-0">
-                            <Config style={iconStyle} />
-                            <span className="d-inline d-sm-none d-lg-inline ms-2 ms-sm-0 ms-lg-2 align-middle">Config</span>
-                        </Nav.Link>
+                        <LinkContainer to="/config">
+                            <Nav.Link className="ms-1 py-0">
+                                <Config style={iconStyle} />
+                                <span className="d-inline d-sm-none d-lg-inline ms-2 ms-sm-0 ms-lg-2 align-middle">Config</span>
+                            </Nav.Link>
+                        </LinkContainer>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
