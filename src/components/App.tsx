e894abeca7eab26a89f60c6dc8f9ef1d57304b75
Otchy <id+github@otchy.net>
2022-01-17 22:20:07 +0900

    0.0.54: Introduce GlobalStyle

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,6 +2,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import { Container } from 'react-bootstrap';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { createGlobalStyle } from 'styled-components';
 import { Api } from '../utils/Api';
 import Footer from './organisms/Footer';
 import Header from './organisms/Header';
@@ -15,32 +16,46 @@ import ApiProvider from './providers/ApiProvider';
 import SearchQueryProvider from './providers/SearchQueryProvier';
 import ToastProvider from './providers/ToastsProvider';
 
+const GlobalStyle = createGlobalStyle`
+    :root {
+        scroll-behavior: unset;
+    }
+    #root {
+        display: flex;
+        flex-direction: column;
+        min-height: 100vh;
+    }
+`;
+
 type Props = {
     apiHost: string;
 };
 
 const App: React.FC<Props> = ({ apiHost }) => {
     return (
-        <BrowserRouter>
-            <ApiProvider api={new Api(apiHost)}>
-                <ToastProvider>
-                    <SearchQueryProvider>
-                        <Header />
-                        <Toasts />
-                        <Container className="mb-auto" style={{ marginTop: '60px' }}>
-                            <Routes>
-                                <Route path="/" element={<HomePage />} />
-                                <Route path="/config" element={<ConfigPage />} />
-                                <Route path="/search" element={<SearchPage />} />
-                                <Route path="/view" element={<ViewPage />} />
-                                <Route path="*" element={<NotFoundPage />} />
-                            </Routes>
-                        </Container>
-                        <Footer />
-                    </SearchQueryProvider>
-                </ToastProvider>
-            </ApiProvider>
-        </BrowserRouter>
+        <>
+            <GlobalStyle />
+            <BrowserRouter>
+                <ApiProvider api={new Api(apiHost)}>
+                    <ToastProvider>
+                        <SearchQueryProvider>
+                            <Header />
+                            <Toasts />
+                            <Container className="mb-auto" style={{ marginTop: '60px' }}>
+                                <Routes>
+                                    <Route path="/" element={<HomePage />} />
+                                    <Route path="/config" element={<ConfigPage />} />
+                                    <Route path="/search" element={<SearchPage />} />
+                                    <Route path="/view" element={<ViewPage />} />
+                                    <Route path="*" element={<NotFoundPage />} />
+                                </Routes>
+                            </Container>
+                            <Footer />
+                        </SearchQueryProvider>
+                    </ToastProvider>
+                </ApiProvider>
+            </BrowserRouter>
+        </>
     );
 };
 
