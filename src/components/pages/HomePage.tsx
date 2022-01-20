e16da9fe75e9969318eda0613d7b1a460931e3a8
Otchy <id+github@otchy.net>
2022-01-21 00:44:50 +0900

    0.0.68: Add All tags section

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -1,14 +1,18 @@
 import { VideoDocument } from '@otchy/home-tube-api/dist/types';
 import React, { useEffect, useState } from 'react';
+import { Col, Row } from 'react-bootstrap';
 import { getSortedVideos } from '../../utils/sortVideos';
 import VideoAlbum from '../organisms/VideoAlbum';
+import { useAllTags } from '../providers/AllTagsProvider';
 import { useApi } from '../providers/ApiProvider';
 import { useHomePageQuery } from '../providers/HomePageQueryProvider';
+import StaticTag from '../atoms/StaticTag';
 
 const HomePage: React.FC = () => {
     const [videos, setVideos] = useState<VideoDocument[] | undefined>();
     const { homePageQuery, setPage } = useHomePageQuery();
     const api = useApi();
+    const { allTags, sortedTags } = useAllTags();
     useEffect(() => {
         api.search().then((videoSet) => {
             setVideos(getSortedVideos(videoSet));
@@ -17,7 +21,20 @@ const HomePage: React.FC = () => {
     const onClickPage = (page: number) => {
         setPage(String(page));
     };
-    return <VideoAlbum videos={videos} page={homePageQuery.page ? parseInt(homePageQuery.page) : 1} onClickPage={onClickPage} />;
+    return (
+        <>
+            <VideoAlbum videos={videos} page={homePageQuery.page ? parseInt(homePageQuery.page) : 1} onClickPage={onClickPage} />
+            <Row>
+                <Col xs={12}>
+                    <h1>All tags</h1>
+                    {sortedTags.length === 0 && 'No tags are defined'}
+                    {sortedTags.map((tag) => {
+                        return <StaticTag tag={tag} count={allTags[tag]} />;
+                    })}
+                </Col>
+            </Row>
+        </>
+    );
 };
 
 export default HomePage;
