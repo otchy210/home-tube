8a778e7224d151db4396c65959134839d5854a81
Otchy <id+github@otchy.net>
2022-02-11 21:20:21 +0900

    0.0.110: Make whole video card unclickable but only image and title clickable

--- src/components/molecules/VideoCard.tsx
+++ src/components/molecules/VideoCard.tsx
@@ -5,25 +5,32 @@ import { createSearchParams, Link } from 'react-router-dom';
 import VideoCardImg from '../atoms/VideoCardImg';
 import StarsIndicator from './StarsIndicator';
 
+const noDecorationStyle = { textDecoration: 'none', color: 'inherit' };
+
 type Props = {
     video: VideoValues;
 };
 
 const VideoCard: React.FC<Props> = ({ video }: Props) => {
     const { key, name, stars } = video;
+    const viewUrl = `/view?${createSearchParams({ key })}`;
     return (
-        <Link to={`/view?${createSearchParams({ key })}`} style={{ textDecoration: 'none', color: 'inherit' }}>
-            <Card className="mt-4">
+        <Card className="mt-4">
+            <Link to={viewUrl} style={noDecorationStyle}>
                 <VideoCardImg video={video} />
-                <Card.Body>
-                    <Card.Title className="fs-6 text-truncate">{name}</Card.Title>
-                    <Card.Subtitle className="text-muted">
-                        <StarsIndicator size={16} stars={stars} />
-                    </Card.Subtitle>
-                    {/* <Card.Text>Some quick example text to build on the card title and make up the bulk of the card's content.</Card.Text> */}
-                </Card.Body>
-            </Card>
-        </Link>
+            </Link>
+            <Card.Body>
+                <Card.Title className="fs-6 text-truncate" title={name}>
+                    <Link to={viewUrl} style={noDecorationStyle}>
+                        {name}
+                    </Link>
+                </Card.Title>
+                <Card.Subtitle className="text-muted">
+                    <StarsIndicator size={16} stars={stars} />
+                </Card.Subtitle>
+                {/* <Card.Text>Some quick example text to build on the card title and make up the bulk of the card's content.</Card.Text> */}
+            </Card.Body>
+        </Card>
     );
 };
 
