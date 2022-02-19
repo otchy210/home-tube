83d93cc1769c17c3b06ac1943a9d11bdaa1553fd
Otchy <id+github@otchy.net>
2022-02-20 07:27:08 +0900

    0.1.10: Don't show all tags until videos are available in HomePage

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -24,7 +24,7 @@ const HomePage: React.FC = () => {
     return (
         <>
             <VideoAlbum videos={videos} page={homePageQuery.page ? parseInt(homePageQuery.page) : 1} onClickPage={onClickPage} />
-            <AllTags />
+            {videos && <AllTags />}
         </>
     );
 };
