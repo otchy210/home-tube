afd1e83c49b98154950aa5751318b6fd163c63bf
Otchy <id+github@otchy.net>
2022-03-10 19:11:58 +0900

    0.4.2: Make NotFoundPage design slightly better

--- src/components/pages/NotFoundPage.tsx
+++ src/components/pages/NotFoundPage.tsx
@@ -3,7 +3,7 @@ import { useI18n } from '../providers/I18nProvider';
 
 const NotFoundPage: React.FC = () => {
     const { t } = useI18n();
-    return <div>{t('Page not found.')}</div>;
+    return <div className="mt-4">{t('Page not found.')}</div>;
 };
 
 export default NotFoundPage;
