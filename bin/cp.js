41cd0c4b4d9aecbacafc1ef94f96b254b16b25ce
Otchy <id+github@otchy.net>
2022-03-17 00:04:08 +0900

    0.5.9: Fix the bug that cp.js doesn't work initially

--- bin/cp.js
+++ bin/cp.js
@@ -1,9 +1,9 @@
 const fs = require('fs');
 const locales = require('../i18next-locales');
 
+fs.mkdirSync('dist/locales', { recursive: true });
 fs.copyFileSync('src/index.html', 'dist/index.html');
 fs.copyFileSync('src/images/favicon.png', 'dist/favicon.png');
-fs.mkdirSync('dist/locales', { recursive: true });
 locales.forEach((lang) => {
     fs.copyFileSync(`src/locales/${lang}.json`, `dist/locales/${lang}.json`);
 });
