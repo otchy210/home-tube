ae5257cb31f4c0a48dc3fe11b7798b0c50783acf
Otchy <id+github@otchy.net>
2022-04-24 16:45:15 +0900

    0.7.22: Truncate card title text only when VideoTable has multiple columns

--- src/components/molecules/VideoCard.tsx
+++ src/components/molecules/VideoCard.tsx
@@ -1,9 +1,18 @@
 import { VideoValues } from '@otchy/home-tube-api/dist/types';
 import React from 'react';
 import { createSearchParams, Link } from 'react-router-dom';
+import styled from 'styled-components';
 import VideoCardImg from '../atoms/VideoCardImg';
 import { Card, CardBody, CardSubtitle, CardTitle } from '../common/card';
 import StarsIndicator from './StarsIndicator';
+
+const TextTruncateCardTitle = styled(CardTitle)`
+    @media (min-width: 768px) {
+        overflow: hidden;
+        text-overflow: ellipsis;
+        white-space: nowrap;
+    }
+`;
 
 const noDecorationStyle = { textDecoration: 'none', color: 'inherit' };
 
@@ -21,11 +30,11 @@ const VideoCard: React.FC<Props> = ({ video }: Props) => {
                 <VideoCardImg video={video} />
             </Link>
             <CardBody>
-                <CardTitle className="fs-6 text-truncate" title={name}>
+                <TextTruncateCardTitle className="fs-6" title={name}>
                     <Link to={viewUrl} style={noDecorationStyle}>
                         {name}
                     </Link>
-                </CardTitle>
+                </TextTruncateCardTitle>
                 <CardSubtitle className="text-muted">
                     <StarsIndicator size={16} stars={stars} />
                 </CardSubtitle>
