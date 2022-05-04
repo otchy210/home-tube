7598a89c08e95f14051daff90269206632d474e8
Otchy <id+github@otchy.net>
2022-05-04 14:05:08 +0900

    0.9.10: Fix svg component properties

--- src/components/images/StarSelectedSvg.tsx
+++ src/components/images/StarSelectedSvg.tsx
@@ -8,8 +8,8 @@ export const StarSelectedSvg: React.FC<SvgProps> = (props) => {
             <path
                 d="M23.063 19.297 L29.234 12.703 20.375 10.984 15.984 3.078 11.625 10.984 2.75 12.703 8.922 19.297 7.813 28.281 15.984 24.438 24.172 28.281 Z"
                 stroke="#f90"
-                stroke-width="2"
-                stroke-linecap="round"
+                strokeWidth="2"
+                strokeLinecap="round"
                 fill="#f90"
             />
         </svg>
